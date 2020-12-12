1892093b30b4ab3585fa5fb9e5925b3f11a89747
ealionel <helitobide@gmail.com>
2020-12-12 22:48:12 +0100

    Day 12 part 2

--- 12/solution.cpp
+++ 12/solution.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <cmath>
 #include <iostream>
 #include <string>
 #include <unordered_map>
@@ -23,6 +24,7 @@ unordered_map<char, Direction> dir_mapping = {
 struct ShipState {
     int pos[2] = {0, 0};  // X, Y;
     Direction pointing = EAST;
+    int waypoint_pos[2] = {10, 1};
 };
 
 array<array<int, 2>, 4> dir_arr = {{
@@ -57,34 +59,70 @@ void move_ship(ShipState &ship, Direction dir, int val) {
     ship.pos[1] += moves[1] * val;
 }
 
-void move_ship_second(ShipState &ship, Direction dir, int val) {}
+void move_waypoint(ShipState &ship, Direction dir, int val) {
+    auto moves = dir_arr[dir];
+
+    ship.waypoint_pos[0] += moves[0] * val;
+    ship.waypoint_pos[1] += moves[1] * val;
+}
+
+void rotate_waypoint(ShipState &ship, Direction dir, int val) {
+    int nb_rotations = val / 90;
+
+    int rot_dir = (dir == LEFT ? -1 : 1) * (nb_rotations == 3 ? -1 : 1);
+
+    if (nb_rotations == 2) {
+        ship.waypoint_pos[0] *= -1;
+        ship.waypoint_pos[1] *= -1;
+    } else {
+        int prev[2] = {ship.waypoint_pos[0], ship.waypoint_pos[1]};
+        ship.waypoint_pos[0] = prev[1] * rot_dir;
+        ship.waypoint_pos[1] = -prev[0] * rot_dir;
+    }
+}
+
+void move_forward(ShipState &ship, int val) {
+    ship.pos[0] += ship.waypoint_pos[0] * val;
+    ship.pos[1] += ship.waypoint_pos[1] * val;
+}
 
 void print_ship(ShipState ship) {
-    printf("EAST: %d, NORTH: %d, POINTING: %d\n", ship.pos[0], ship.pos[1],
-           ship.pointing);
+    printf("EAST: %d, NORTH: %d, POINTING: %d, WAYPOINT: (%d, %d) \n",
+           ship.pos[0], ship.pos[1], ship.pointing, ship.waypoint_pos[0],
+           ship.waypoint_pos[1]);
 }
 
 int main() {
     string line;
 
     ShipState ship;
+    ShipState second_ship;
 
-    print_ship(ship);
+    // print_ship(ship);
 
     while (cin >> line) {
         auto parsed = parse_instruction(line);
 
+        cout << line << "\t";
         Direction cmd = parsed.first;
         int val = parsed.second;
 
         if (cmd == LEFT || cmd == RIGHT) {
             rotate_ship(ship, cmd, val);
+            rotate_waypoint(second_ship, cmd, val);
         } else {
             move_ship(ship, cmd, val);
+
+            if (cmd == FORWARD) {
+                move_forward(second_ship, val);
+            } else {
+                move_waypoint(second_ship, cmd, val);
+            }
         }
-        cout << line << "\t";
-        print_ship(ship);
+        print_ship(second_ship);
     }
 
     cout << "Solution is " << abs(ship.pos[0]) + abs(ship.pos[1]) << endl;
+    cout << "Solution is " << abs(second_ship.pos[0]) + abs(second_ship.pos[1])
+         << endl;
 }
